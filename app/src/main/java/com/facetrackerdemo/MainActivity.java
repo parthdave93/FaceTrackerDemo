25807f72b687fc5d08f20fc7cfd4f22b3c63b5e4
Parth Dave <parth.dave93@gmail.com>
2017-06-15 16:03:04 +0530

    take a snap from camera

--- app/src/main/java/com/facetrackerdemo/MainActivity.java
+++ app/src/main/java/com/facetrackerdemo/MainActivity.java
@@ -6,9 +6,13 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,7 +26,12 @@ import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static android.os.Environment.getExternalStorageDirectory;
+import static java.io.File.separator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private CameraOverlay cameraOverlay;
     private static final int RC_HANDLE_GMS = 9001;
     private static final int RC_HANDLE_CAMERA_PERM = 2;
+    public static final int APP_PERMISSION = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -126,27 +136,40 @@ public class MainActivity extends AppCompatActivity {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             return;
         }
-
-        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "Camera permission granted - initialize the camera source");
-            createCameraSource();
-            return;
+        for (int i = 0, len = permissions.length; i < len; i++) {
+    
+            String permission = permissions[i];
+            if (Manifest.permission.CAMERA.equals(permission) && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Camera permission granted - initialize the camera source");
+                createCameraSource();
+                return;
+            }else{
+                DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                };
+    
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("FaceTrackerDemo")
+                       .setMessage("Need Camera access permission!")
+                       .setPositiveButton("OK", listener)
+                       .show();
+            }
+            if (Manifest.permission.WRITE_EXTERNAL_STORAGE.equals(permission) && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                onCaptureClick(null);
+            }else{
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("FaceTrackerDemo")
+                       .setMessage("Need Storage access permission!")
+                       .setPositiveButton("OK",null)
+                       .show();
+            }
         }
-
+    
         Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
                 " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
 
-        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                finish();
-            }
-        };
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("FaceTrackerDemo")
-                .setMessage("Need Camera access permission!")
-                .setPositiveButton("OK", listener)
-                .show();
     }
 
 
@@ -206,5 +229,52 @@ public class MainActivity extends AppCompatActivity {
         public void onDone() {
             mOverlay.remove(faceOverlayGraphics);
         }
+    }
+    
+    public void onCaptureClick(View view){
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            takeSnap();
+        }else{
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                              APP_PERMISSION);
+        }
+       
+    }
+    
+    private void takeSnap() {
+        mCameraSource.takePicture(new CameraSource.ShutterCallback() {
+            @Override public void onShutter() {
+                //make a shutter sound if you want to
+            }
+        }, new CameraSource.PictureCallback() {
+            @Override public void onPictureTaken(byte[] bytes) {
+                try {
+                    String directoryPath = getExternalStorageDirectory() + separator + "Dummy_path"+ separator;
+                    File directory = new File(directoryPath);
+                    //create directory if not exists
+                    if (!directory.exists())
+                        directory.mkdirs();
+                    File captureFile = new File(directoryPath + "img_" + System.currentTimeMillis() + ".jpg");
+                    
+                    //create file if not exists
+                    if (!captureFile.exists())
+                        captureFile.createNewFile();
+                    FileOutputStream stream = new FileOutputStream(captureFile);
+                    stream.write(bytes);
+                    stream.flush();
+                    stream.close();
+    
+                    //opening that file replace below code with what you want to do
+                    Intent intent = new Intent();
+                    intent.setAction(android.content.Intent.ACTION_VIEW);
+                    intent.setDataAndType(Uri.fromFile(captureFile), "image/*");
+                    startActivity(intent);
+                    finish();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
